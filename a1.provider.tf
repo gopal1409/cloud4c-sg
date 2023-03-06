58b65fd715e2013e3e2b175a4daed31fd434db8e
gopal das <gopal1409@gmail.com>
2023-03-06 20:49:57 +0530

    terraform sg

--- a1.provider.tf
+++ a1.provider.tf
@@ -13,20 +13,20 @@ terraform {
     null = {
       source = "hashicorp/null"
     }
-    backend "remote" {
-      hostname     = "app.terraform.io"
-      organization = "cloud4cvpc"
-      workspace {
-        prefix = "vpc-"
-      }
-    }
+
 
   }
 
-  ####
+  backend "remote" {
+    hostname     = "app.terraform.io"
+    organization = "cloud4cvpc"
+    workspace {
+      prefix = "vpc-"
+    }
+  }
 
 }
-#provider block
+
 provider "aws" {
   region = var.aws_region
 }
