0dfddfc181f188c545ffb92325f69993742a18c0
gopal das <gopal1409@gmail.com>
2023-03-06 20:58:22 +0530

    terraform sg

--- a5-1.sg-bastion.tf
+++ a5-1.sg-bastion.tf
@@ -3,7 +3,7 @@ module "public_bastion_sg" {
   version = "4.17.1"
   ###name of the sg need to be changes
   name                = "public-bastion-sg-gopal"
-  vpc_id              = module.vpc.vpc_id
+  vpc_id              = data.terraform_remote_state.vpc.vpc_id
   ingress_rules       = ["ssh-tcp"]
   ingress_cidr_blocks = ["0.0.0.0/0"]
   egress_rules        = ["all-all"]
