9910ab38cd36b35d5e4bcce5354cef54933d2e1a
gopal das <gopal1409@gmail.com>
2023-03-06 20:28:47 +0530

    security group

---
+++ a1.provider.tf
@@ -0,0 +1,31 @@
+###terraform setting block
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      #version = "~> 4.0"
+      #in production always put this version number
+    }
+    ###adding backend as s3 for remote state storage
+    random = {
+      source = "hashicorp/random"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+    backend "remote" {
+      hostname="app.terraform.io"
+      organization = "cloud4cvpc" 
+      workspace {
+        prefix = "vpc-"
+      }
+    }
+
+  }
+  ####
+
+}
+#provider block
+provider "aws" {
+  region = var.aws_region
+}
